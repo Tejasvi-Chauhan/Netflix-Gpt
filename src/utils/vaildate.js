266cf8790d2742fd2e6b4db9e8a7ff7514fab719
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-01-10 23:14:27 +0530

    update userSlice

--- src/utils/vaildate.js
+++ src/utils/vaildate.js
@@ -3,7 +3,7 @@
 const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
 const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
 
- if(!isEmailValid) return "Email ID is not Valid";
+ if(!isEmailValid) return "Emailis not Valid";
  if(!isPasswordValid) return "Password is not valid";
 
  return null;
